54d0323b8851692fdea92807eb87332315565676
Devon-1984 <deb100.db@gmail.com>
2025-04-30 00:20:30 +0530

    idk

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,19 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
+const prismaClientSingleton = () =>
+  new PrismaClient({
     log: ["query", "error"],
   });
-};
 
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma;
+}
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+export default prisma;

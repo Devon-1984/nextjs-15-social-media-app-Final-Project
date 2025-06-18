8b7de0d3391365fae20f6d86312a084b7ea8cf68
Devon-1984 <deb100.db@gmail.com>
2025-06-19 00:49:44 +0530

    Title change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,10 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | bugbook",
-    default: "bugbook",
+    template: "%s | Alumni Connect",
+    default: "Alumni Connect",
   },
-  description: "The social media app for powernerds",
+  description: "The social media app for SIT grads",
 };
 
 export default function RootLayout({
